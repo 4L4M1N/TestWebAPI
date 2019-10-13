dabd4f4f0daab65044368e516e57b6cddd8f7722
Mohammad Al Amin Hossain <mohammadamin601@live.com>
2019-10-13 16:40:30 +0600

    initial

---
+++ LearnAPI/Controllers/AuthController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using LearnAPI.Models;
+using LearnAPI.Repositories;
+using Microsoft.AspNet.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.OAuth;
+
+
+namespace LearnAPI.Controllers
+{
+    [RoutePrefix("api/Auth")]
+    public class AuthController : ApiController
+    {
+        private AuthRepository _repo = null;
+
+        public AuthController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(ApplicationUser _user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(_user);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+
+            //Security Token
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new System.Security.Claims.Claim("sub", _user.UserName));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes("amaamamamasisdfwefdsfdfgwerf123".ToString())); //Set Secret value
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Insert info to token
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(identity),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LearnAPI/Repositories/AuthRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.WebSockets;
+using LearnAPI.Data;
+using LearnAPI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace LearnAPI.Repositories
+{
+    public class AuthRepository : IDisposable
+    {
+        private ApplicationDbContext _context;
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser,string> _signInManager;
+
+        public AuthRepository()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+        }
+        public async Task<IdentityResult> RegisterUser(ApplicationUser _user)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = _user.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, _user.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            _userManager.Dispose();
+        }
+    }
+}
